4874d45280ff07ffd44f54cdfc527ef3a131bccc
MahamoudAH <mahamoud.ali.hassan19@gmail.com>
2023-10-22 23:01:45 +0200

    part one

--- Hangman/main.py
+++ Hangman/main.py
@@ -2,14 +2,19 @@ import random
 from hangman_art import logo, stages
 from hangman_words import word_list
 
-
+print(logo)
 guess = input("Guess a letter: ")
 
 word = random.choice(word_list)
+i = len(stages)
 
-if guess in word:
-    print("Yes")
-else:
-    print(f"You guessed {guess}, that's not in the word. You lose a life.")
+while i >= 1:
+    if guess in word:
+        guess = input("Guess a letter: ")
+    else:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
 
-print(word)
+    i -= 1
+    print(i)
+
+# print(word)
