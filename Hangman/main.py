837860c718116a305400fab183e7ed111d2a0e28
MahamoudAH <mahamoud.ali.hassan19@gmail.com>
2023-10-22 23:31:44 +0200

    end day

--- Hangman/main.py
+++ Hangman/main.py
@@ -6,15 +6,28 @@ print(logo)
 guess = input("Guess a letter: ")
 
 word = random.choice(word_list)
-i = len(stages)
+liste = []
+for i in range(1, len(stages)):
+    liste.append('_')
+
+i = len(stages) - 1
+
+liste = ' '.join(liste)
+print(liste)
+
+print(word)
 
 while i >= 1:
     if guess in word:
+        index = word.index(guess)
+        print(liste)
+        print(stages[i])
         guess = input("Guess a letter: ")
     else:
         print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        print(liste)
+        print(stages[i])
+        guess = input("Guess a letter: ")
 
     i -= 1
     print(i)
-
-# print(word)
