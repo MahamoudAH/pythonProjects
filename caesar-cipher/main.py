c0da8d114d30351f6cd65114391f149956d8a098
MahamoudAH <mahamoud.ali.hassan19@gmail.com>
2023-10-24 22:00:39 +0200

    Version 2

--- caesar-cipher/main.py
+++ caesar-cipher/main.py
@@ -1,7 +1,9 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
             'z']
 
-message = input("Type your message: ")
+user_choice = input(
+    "Type 'encode' to encrypt, type 'decode' to decrypt: ").lower()
+message = input("Type your message: ").lower()
 shift = int(input("Type the shift number: "))
 
 new = alphabet[shift:]
@@ -9,11 +11,21 @@ av = alphabet[0:shift]
 
 alphabet_deca = new + av
 
+
 new_message = []
-for letter in message:
-    idx = alphabet.index(letter)
-    new_message.append(alphabet_deca[idx])
+
+if user_choice == 'encode':
+    for letter in message:
+        if letter in alphabet:
+            new_message.append(alphabet_deca[alphabet.index(letter)])
+        else:
+            new_message.append(letter)
+elif user_choice == 'decrypt':
+    for letter in message:
+        if letter in alphabet_deca:
+            new_message.append(alphabet[alphabet_deca.index(letter)])
+        else:
+            new_message.append(letter)
 
 result = "".join(new_message)
-print(new_message)
 print(result)
