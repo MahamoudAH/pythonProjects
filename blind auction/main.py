ee4526fc6ddd666f0eca42a326f31f173d8a29ee
MahamoudAH <mahamoud.ali.hassan19@gmail.com>
2023-10-25 23:25:41 +0200

    version 2

--- blind auction/main.py
+++ blind auction/main.py
@@ -7,3 +7,12 @@ while game != "no":
     bid = int(input("What's your bid? "))
     auction[name] = bid
     game = input("Are there any other bidders? Type 'yes or 'no': ")
+
+winner_bid = 0
+winner_name = ""
+for key, value in auction.items():
+    if value > winner_bid:
+        winner_bid = value
+        winner_name = key
+
+print(f"The winner is {winner_name} a bid of ${winner_bid}")
